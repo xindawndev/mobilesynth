56e16d36949984975dfe99465838a8451d0ee39b
allen.porter <allen.porter@9f3eb83e-ca77-11dd-a2ab-b939405fd53f>
2009-02-22 18:48:58 +0000

    New fixed point routines, constants, and more tests.

--- mobilesynth/Classes/synth/fixed_point.h
+++ mobilesynth/Classes/synth/fixed_point.h
@@ -12,7 +12,13 @@ namespace synth {
 #define TOFIX(d) ((int)((d) * (double)(1 << 24)))
 #define TOFLT(a) ((double)(a) / (double)(1 << 24))
 
+static const int kFixedMinus1 = TOFIX(-1.0);
+static const int kFixedHalf = TOFIX(0.5);
+static const int kFixed1 = TOFIX(1.0);
+static const int kFixed2 = TOFIX(2.0);
+
 // 8.24 fixed point multiplication
-int fixed_mult(int v1, int v2);
+int32_t fixed_mult(int32_t v1, int32_t v2);
+int32_t fixed_floor(int32_t v);
 
 }  // namespace synth
