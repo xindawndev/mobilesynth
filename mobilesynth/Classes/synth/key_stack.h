f5bafd683e29c77b632ffcbca52ba812b65f6bdb
allen.porter <allen.porter@9f3eb83e-ca77-11dd-a2ab-b939405fd53f>
2009-03-16 01:23:34 +0000

    Add a key stack that keeps track of notes pressed and released, in order to help fix issue #19: http://code.google.com/p/mobilesynth/issues/detail?id=19 BUG=19

---
+++ mobilesynth/Classes/synth/key_stack.h
@@ -0,0 +1,34 @@
+// key_stack.h
+// Author: Allen Porter <allen@thebends.org>
+//
+// The key stack keeps track of notes pressed and released, determining when
+// the attack phase should be entered or when the note should just change
+// without starting a new note attack.
+
+#ifndef __KEY_STACK_H__
+#define __KEY_STACK_H__
+
+#include <vector>
+
+namespace synth {
+
+class KeyStack {
+ public:
+  // Returns true if this was the first note pushed on to the key stack
+  bool NoteOn(int note);
+
+  // Returns true if this was the last note removed from the key stack
+  bool NoteOff(int note);
+
+  // Returns the current not, or 0 if no note is playing.
+  int GetCurrentNote();
+
+  int size() { return notes_.size(); }
+
+ private:
+  std::vector<int> notes_;
+};
+
+}  // namespace synth
+
+#endif  // __KEY_STACK_H__
---
+++ mobilesynth/Classes/synth/key_stack_test.cpp
@@ -0,0 +1,52 @@
+// key_stack_test.cpp
+// Author: Allen Porter <allen@thebends.org>
+
+#include <assert.h>
+#include <iostream>
+#include <vector>
+#include "synth/key_stack.h"
+
+namespace {
+
+static void TestSimple() {
+  synth::KeyStack stack;
+  assert(0 == stack.GetCurrentNote());
+  stack.NoteOn(1);
+  assert(1 == stack.GetCurrentNote());
+  assert(1 == stack.size());
+  stack.NoteOff(1);
+  assert(0 == stack.GetCurrentNote());
+  assert(0 == stack.size());
+}
+
+static void TestMultiple() {
+  synth::KeyStack stack;
+  assert(0 == stack.GetCurrentNote());
+  stack.NoteOn(1);
+  assert(1 == stack.GetCurrentNote());
+  assert(1 == stack.size());
+  stack.NoteOn(2);
+  assert(2 == stack.GetCurrentNote());
+  assert(2 == stack.size());
+  stack.NoteOff(1);
+  assert(2 == stack.GetCurrentNote());
+  assert(1 == stack.size());
+  stack.NoteOn(3);
+  assert(3 == stack.GetCurrentNote());
+  assert(2 == stack.size());
+  stack.NoteOff(3);
+  assert(2 == stack.GetCurrentNote());
+  assert(1 == stack.size());
+  stack.NoteOff(2);
+  assert(0 == stack.GetCurrentNote());
+  assert(0 == stack.size());
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  TestSimple();
+  TestMultiple();
+  std::cout << "PASS" << std::endl;
+  return 0;
+}
