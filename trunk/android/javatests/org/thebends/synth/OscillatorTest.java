be83de49aec46ed33ed1c809c5e74f152c0b2c03
allen.porter <allen.porter@9f3eb83e-ca77-11dd-a2ab-b939405fd53f>
2009-11-23 05:48:47 +0000

    More ported tests.

--- trunk/android/javatests/org/thebends/synth/OscillatorTest.java
+++ trunk/android/javatests/org/thebends/synth/OscillatorTest.java
@@ -16,4 +16,92 @@ public class OscillatorTest extends TestCase {
       assertEquals(-1.0, osc.getValue(), TOLERANCE);
     }
   }
+
+  public void testSquare() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(1.0), 7.0f);
+    osc.setWaveForm(Oscillator.WaveForm.SQUARE);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+    }
+  }
+
+  public void testSawtooth() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(1.0), 8.0f);
+    osc.setWaveForm(Oscillator.WaveForm.SAWTOOTH);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-0.75, osc.getValue(), TOLERANCE);
+      assertEquals(-0.5, osc.getValue(), TOLERANCE);
+      assertEquals(-0.25, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.25, osc.getValue(), TOLERANCE);
+      assertEquals(0.5, osc.getValue(), TOLERANCE);
+      assertEquals(0.75, osc.getValue(), TOLERANCE);
+    }
+  }
+
+  public void testSawtooth2() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(2.0), 8.0f);
+    osc.setWaveForm(Oscillator.WaveForm.SAWTOOTH);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-0.5, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.5, osc.getValue(), TOLERANCE);
+    }
+  }
+
+  public void testReverseSawtooth() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(1.0), 8.0f);
+    osc.setWaveForm(Oscillator.WaveForm.REVERSE_SAWTOOTH);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.75, osc.getValue(), TOLERANCE);
+      assertEquals(0.5, osc.getValue(), TOLERANCE);
+      assertEquals(0.25, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(-0.25, osc.getValue(), TOLERANCE);
+      assertEquals(-0.5, osc.getValue(), TOLERANCE);
+      assertEquals(-0.75, osc.getValue(), TOLERANCE);
+    }
+  }
+
+  public void testTriangle() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(1.0), 8.0f);
+    osc.setWaveForm(Oscillator.WaveForm.TRIANGLE);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.5, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(-0.5, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(-0.5, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.5, osc.getValue(), TOLERANCE);
+    }
+  }
+
+  public void testTriangle2() throws Exception {
+    Oscillator osc = new Oscillator(FixedParameter.get(2.0), 8.0f);
+    osc.setWaveForm(Oscillator.WaveForm.TRIANGLE);
+
+    for (int i = 0; i < 10; ++i) {
+      assertEquals(1.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+      assertEquals(-1.0, osc.getValue(), TOLERANCE);
+      assertEquals(0.0, osc.getValue(), TOLERANCE);
+    }
+  }
+
 }
