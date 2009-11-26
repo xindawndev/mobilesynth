3711feb52402bce4ec7d4184fb7e9e1cb9242fd3
allen.porter <allen.porter@9f3eb83e-ca77-11dd-a2ab-b939405fd53f>
2009-11-26 08:05:14 +0000

    Adds "Hello World" android code, not linked at all to the synth code.

---
+++ trunk/android/java/org/thebends/mobilesynth/MobileSynthActivity.java
@@ -0,0 +1,15 @@
+package org.thebends.mobilesynth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileSynthActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
