4878f22a54f829312c7687a7a2848ddfe262d44d
allen.porter <allen.porter@9f3eb83e-ca77-11dd-a2ab-b939405fd53f>
2009-11-27 20:52:21 +0000

    Buffer improvements, shutdown audio when the activity is paused.

--- trunk/android/java/org/thebends/mobilesynth/MobileSynthActivity.java
+++ trunk/android/java/org/thebends/mobilesynth/MobileSynthActivity.java
@@ -5,19 +5,43 @@ import android.os.Bundle;
 import org.thebends.synth.Oscillator;
 import org.thebends.synth.FixedParameter;
 
-public class MobileSynthActivity extends Activity
-{
-    private Oscillator osc;
-    private SynthTrack t;
+import java.util.logging.Logger;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class MobileSynthActivity extends Activity {
+  private static Logger LOG = Logger.getLogger(
+      MobileSynthActivity.class.getSimpleName());
 
-        osc = new Oscillator(FixedParameter.get(1000), 44100);
-        t = new SynthTrack(osc);
-    }
+  private Oscillator osc;
+  private SynthTrack t;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    LOG.info("Creating synth activity");
+    // TODO(allen): Layout the keyboard
+    setContentView(R.layout.main);
+
+    osc = new Oscillator(FixedParameter.get(1000), 44100);
+    osc.setWaveForm(Oscillator.WaveForm.SAWTOOTH);
+  }
+
+  private void play() {
+    t = new SynthTrack(osc);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    LOG.info("Pausing synth activity");
+    t.shutdown();
+    t = null;
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    LOG.info("Resuming synth activity");
+    play();
+  }
 }
